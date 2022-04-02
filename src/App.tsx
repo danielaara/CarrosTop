063ec1e59963740cc421f97403bd9cdfaa94875a
Daniela <antonia.araujo74@aluno.ce.gov.br>
2022-04-02 16:01:27 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './assets/styles/custom.scss'
+import './App.css';
+
+import BRoutes from 'Routes';
+
+const App = () =>{
+  return (
+    <BRoutes/>
+
+    
+  );
+}
+
+export default App;
---
+++ src/Routes.tsx
@@ -0,0 +1,21 @@
+import Navbar from "components/Navbar";
+import Catalog from "pages/Catalog";
+import Home from "pages/Home";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+const BRoutes = () =>(
+        <BrowserRouter>
+        <Navbar />
+           <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Catalog />} />
+
+
+           </Routes>
+        </BrowserRouter>
+
+
+    );
+
+export default BRoutes;
